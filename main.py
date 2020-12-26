c434c444131684cbbf20c5316ca60e602f5230bb
exprms <72883406+exprms@users.noreply.github.com>
2020-12-26 19:53:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,22 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import classes
 
 df = pd.read_csv('iris.data', header=None)
+df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 
 # Auswahl setosa und versicolor
-y = df.loc[:, 4].values
+y = df.iloc[:, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 
 # Auswahl Kelch- und Blütenlänge
-X = df.loc[:,[0,2]].values
+X = df.iloc[:,[0,2]].values
 
-# Scatter Plot
-setosa_index = np.where(y==1)
-versicolor_index = np.where(y==-1)
-
-#plt.scatter(X[setosa_index,0], X[setosa_index,1], color='red', marker='o', label='setosa')
-#plt.scatter(X[versicolor_index,0], X[versicolor_index,1], color='blue', marker='o', label='versicolor')
-#plt.xlabel('Laenge Kelchblatt [cm]')
-#plt.legend(loc='upper left')
-#plt.show()
+# scatterplot
+sns.scatterplot(df['sepal_length'], df['sepal_width'], hue=df['species'])
+plt.show()
 
 # Klassifikation
 ppn = classes.Perceptron(eta=0.01, n_iter=10)
